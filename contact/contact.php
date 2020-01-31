0d3363cca4c3f21523111b3680ab115b637de64e
Henrik Krieger <henrikkriiger@gmail.com>
2020-02-01 01:35:35 +0200

    Video preload auto

--- contact/contact.php
+++ contact/contact.php
@@ -17,11 +17,11 @@ $fromEmail = $_POST['email'];
 $fromName = $_POST['name'];
 
 // an email address & name that will receive the email with the output of the form
-$sendToEmail = 'name@mydomain.com';
-$sendToName = 'Name';
+$sendToEmail = 'henrikkriiger@gmail.com';
+$sendToName = 'Henrik Kriiger';
 
 // subject of the email
-$subject = 'New message from contact form';
+$subject = 'New message from (Funktech2020Beta) contact form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
